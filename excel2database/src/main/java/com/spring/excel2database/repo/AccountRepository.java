b03604f837fa506c6bbaec065d01be166be4e51d
kubraucar1 <kubraucr2002@gmail.com>
2024-03-07 16:22:42 +0300

    read excel, save database

---
+++ excel2database/src/main/java/com/spring/excel2database/repo/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.spring.excel2database.repo;
+
+import com.spring.excel2database.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Integer> {
+}
